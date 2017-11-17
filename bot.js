a0b0962772fe493b3c698d01a40002ad1242b4f8
SuperMLGCoder <sahaaritro21@gmail.com>
2017-11-17 09:54:57 -0500

    It will listen to a large galaxy, i THINK

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setPresence({game: {name: "in a large galaxy, --help", type: 1}});
+    client.user.setPresence({game: {name: "in a large galaxy, --help", type: 2}});
     console.log('Ready!');
 });
 
