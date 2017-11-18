ba9d4366f347edf9ba9661e6fa515c13ce48e6e9
SuperMLGCoder <sahaaritro21@gmail.com>
2017-11-18 14:17:51 -0500

    a

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity({game: {name: "in a large galaxy, --help", type: "WATCHING"}});
+    client.user.setActivity({game: {name: "in a large galaxy, --help", type: 1}});
     console.log('Ready!');
 });
 
