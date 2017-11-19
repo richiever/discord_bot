2bd7c152e6a50e35987dc1dc3e5621e255ddea5c
SuperMLGCoder <sahaaritro21@gmail.com>
2017-11-18 21:37:02 -0500

    a

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', message => {
           .addField("ban", "Bans a person from the server (admin only)")
           .addField("you suck (no prefix)", "says 'well you swallow'")
           .setColor("#0000FF")
-      message.channel.send(help_embed).catch(err => console.log(err));
+      message.channel.send({ embed: help_embed }).catch(err => console.log(err));
     }
 
     else if (message.content === '--avatar') {
