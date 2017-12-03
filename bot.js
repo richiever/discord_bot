f29a67c8a04c2f8b9367132daf6ee071e51edbf5
SuperMLGCoder <sahaaritro21@gmail.com>
2017-12-03 16:54:29 -0500

    fixing

--- bot.js
+++ bot.js
@@ -170,19 +170,19 @@ client.on('message', async message => {
         return message.channel.send("Please provide a link / search term of a video.");
       }
       console.log("passed 171");
-      YoutubeSearcher(link, opts, function(err, results) {
-        if(err) return console.log(err);
-        console.log("passed 174");
-        // finds id of video
-        console.dir(results[0].id);
-        videoID = results[0].id;
+      // YoutubeSearcher(link, opts, function(err, results) {
+      //   if(err) return console.log(err);
+      //   console.log("passed 174");
+      //   // finds id of video
+      //   console.dir(results[0].id);
+      //   videoID = results[0].id;
 
-        // find link of video
-        console.dir(results[0].link);
-        link = results[0].link[0];
-      });
+      //   // find link of video
+      //   console.dir(results[0].link);
+      //   link = results[0].link[0];
+      // });
 
-      const songsInfo = await YTDL.getInfo(link);
+      const songsInfo = await YTDL.getInfo(video_arg);
       console.log("passed 185");
       const songs = {
         title: Util.escapeMarkdown(songsInfo.title),
